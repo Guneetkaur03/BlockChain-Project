699de8cc33f387935f160ed1029c76f5c19a5ae0
Guneetkaur03 <guneet.kaur.15cse@bml.edu.in>
2021-12-14 19:51:41 -0600

    repo cleaning

--- other_files/simple_txt_parse.py
+++ other_files/simple_txt_parse.py
@@ -4,7 +4,7 @@ output_f = open("Data/edges2010/outputs2015_1.txt", "r")
 end_time = time.time()
 print("Time taken to load the file is {}".format(end_time - start_time))
 
-lines = f.readlines()
+lines = output_f.readlines()
 print("checking some of the contents of the file, printing top 5 details")
 for i in range(5):
     print(lines[i].split("\t")[0])
